b447ed3bda5ec898fcbdfad66b7abf5badf73f48
Logan-nk <logan@ninjakiwi.com>
2019-07-20 10:06:44 +1200

    committing git ignore

---
+++ RockClimbingGame/Assets/Scripts/Utils/RockGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockGenerator : MonoBehaviour {
+
+	private GameObject rockPrefab = null;
+
+	[ContextMenu("Generate Rocks")]
+	void GenerateRocks() {
+		Debug.Log("Generating Rocks - Start");
+
+		if (rockPrefab == null) {
+			rockPrefab = transform.Find("RockPrefab").gameObject;
+			rockPrefab.SetActive(false);
+		}
+
+
+
+		Debug.Log("Generating Rocks - End");
+	}
+}
