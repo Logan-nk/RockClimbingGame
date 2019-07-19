00058f3b2b468479c07ffc5cd973ec79974247b1
Logan-nk <logan@ninjakiwi.com>
2019-07-20 11:34:16 +1200

    testing a controller

--- RockClimbingGame/Assets/ControllerTest.cs
+++ RockClimbingGame/Assets/ControllerTest.cs
@@ -12,31 +12,25 @@ public class ControllerTest : MonoBehaviour {
     bool xbox_lb;
     bool xbox_rb;
 
-    public Text debugText;
-
     private void Start() {
-        ltaxis = Input.GetAxis("LeftLeg");
-        rtaxis = Input.GetAxis("RightLeg");
-        hAxis = Input.GetAxis("Horizontal");
-        vAxis = Input.GetAxis("Vertical");
-        xbox_lb = Input.GetButton("LeftHand");
-        xbox_rb = Input.GetButton("RightHand");
+        
     }
 
     void ControllerCheck() {
+		ltaxis = Input.GetAxis("LeftLeg");
+		rtaxis = Input.GetAxis("RightLeg");
+		hAxis = Input.GetAxis("Horizontal");
+		vAxis = Input.GetAxis("Vertical");
+		xbox_lb = Input.GetButton("LeftHand");
+		xbox_rb = Input.GetButton("RightHand");
 
-        debugText.text =
-            string.Format(
-                "LTrigger: {0:0.000} RTrigger: {1:0.000}\n" +
-                "Horizontal: {2:0.000} Vertical: {3:0.000}\n" +
-                "LB: {4} RB: {5}",
-                ltaxis, rtaxis,
-                xbox_lb, xbox_rb,
-                hAxis, vAxis);
+
+		Debug.Log(string.Format(
+			"LeftLeg{0:0.00} RightLeg{1:0.00} HAxis {2:0.00} VAxis {3:0.00} LB {4} RB {5}", 
+			ltaxis, rtaxis, hAxis, vAxis, xbox_lb, xbox_rb));
     }
 
     void Update() {
-        // Camera Rig Movement Control
 
         ControllerCheck();
     }
